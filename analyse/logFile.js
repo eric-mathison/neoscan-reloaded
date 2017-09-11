18ff9b6ee5bc5fc86e5530492c55fc2d52164b5a
Herrad <herrad.koe@gmail.com>
2017-09-11 21:33:36 +0100

    wrong location

--- analyse/logFile.js
+++ analyse/logFile.js
@@ -1,5 +1,5 @@
 'use strict'
-const logPath = "C:\\Games\\Neocron 2.2 CE\\Live Server\\logs"
+const logPath = "C:\\Games\\Neocron 2.2 CE\\logs"
 
 module.exports = function createPath(characterName, basePath) {
     basePath = basePath || logPath;
